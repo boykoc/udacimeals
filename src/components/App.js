3689732af473af3446eb0064321e193c3c28b50b
Cody Boyko <codyboyko@yahoo.ca>
2017-10-13 19:06:32 +0000

    Use connect().

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
+    console.log('Props', this.props)
     return (
       <div>
         Hello World.
@@ -10,4 +12,22 @@ class App extends Component {
   }
 }
 
-export default App
+// Return state from redux store as array so we can easily work with it as grid.
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+  
+  return {
+    calendar: dayOrder.map((day) => ({
+      day, 
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+          
+        return meals
+      }, {})
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(App)
